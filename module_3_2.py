ad6127aa09b8b835b0340850d9912ce267cf9d88
RinazTurnin <52930266+RinazTurnin@users.noreply.github.com>
2024-08-14 02:41:37 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,24 @@
+def send_email(message, recipient,* , sender = "university.help@gmail.com"):
+  valid_domians = ['.com', '.net', '.ru']
+  if not('@' in sender and any(sender.endswith(domain) for domain in valid_domians))\
+  and ('@' in recipient and any(recipient.endswith(domain) for domain in valid_domians)):
+    print(f'Невозможно отправить письмо с {sender} на адрес {recipient}') 
+  elif sender == recipient:
+    print('Невозможно отправить письмо самому себе!')
+  elif sender != 'university.help@gmail.com':
+    print(f'НЕСТАНДАНЫЙ ОТПРАВИТЕЛЬ! Невозможно отправить письмо с {sender} на адрес {recipient}')
+  else:
+    print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}!')
+
+
+  
+
+
+
+    
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
